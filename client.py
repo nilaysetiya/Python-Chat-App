d42b8af64c75abf2ab023244821791c35bda8027
Nilay Setiya <87846852+nilaysetiya@users.noreply.github.com>
2022-10-11 15:27:47 +1300

    Added basic login screen

--- client.py
+++ client.py
@@ -1,31 +1,72 @@
 import socket
 import threading
 import sys
-from PyQt5.QtWidgets import QApplication, QWidget, QDialog
-from PyQt5.QtCore import QCoreApplication
+from PyQt5.QtWidgets import *
+from PyQt5.QtCore import *
 
-class MyApp(QWidget):
 
+class MainScreen(QWidget):
     def __init__(self):
         super().__init__()
         self.initUI()
-    
+
     def initUI(self):
         self.setWindowTitle('Chat Client')
         self.setGeometry(750, 250, 1200, 750)
         self.show()
-    
+
 
 class Login(QWidget):
 
     def __init__(self):
         super().__init__()
+
+        self.w = None
+
         self.initUI()
-    
+
     def initUI(self):
+        # IP Address input
+        ip_address_edit = QLineEdit(self)
+        ip_address_edit.setAlignment(Qt.AlignCenter)
+        ip_label = QLabel('Enter server IP', self)
+        ip_label.setAlignment(Qt.AlignCenter)
+
+        # Port input
+        port_edit = QLineEdit(self)
+        port_edit.setAlignment(Qt.AlignCenter)
+        port_label = QLabel('Enter server port', self)
+        port_label.setAlignment(Qt.AlignCenter)
+
+        # Nickname input
+        nickname_edit = QLineEdit(self)
+        nickname_edit.setAlignment(Qt.AlignCenter)
+        nickname_label = QLabel('Enter a nickname', self)
+        nickname_label.setAlignment(Qt.AlignCenter)
+
+        # Connect button
+        connect_button = QPushButton('Connect', self)
+        connect_button.clicked.connect(self.connect_button_on_click)
+
+        vbox = QVBoxLayout()
+        vbox.addWidget(ip_label)
+        vbox.addWidget(ip_address_edit)
+        vbox.addWidget(port_label)
+        vbox.addWidget(port_edit)
+        vbox.addWidget(nickname_label)
+        vbox.addWidget(nickname_edit)
+        vbox.addStretch(1)
+        vbox.addWidget(connect_button)
+
+        self.setLayout(vbox)
+
         self.setWindowTitle('Connect to Server')
-        self.setGeometry(1000, 500, 500, 350)
+        self.setGeometry(750, 250, 500, 350)
         self.show()
+
+    def connect_button_on_click(self):
+        self.w = MainScreen()
+        self.w.show()
 
 
 # # Listen to server
@@ -73,4 +114,3 @@ if __name__ == '__main__':
     except KeyboardInterrupt:
         # client.close()
         sys.exit(app.exec_())
-
